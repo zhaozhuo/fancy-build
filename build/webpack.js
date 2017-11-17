6e88138a9a0c79f950e797fd732bca461c8349ad
Tencent Ubuntu <ubuntu@tencent.dalian>
2017-11-17 18:18:19 +0800

    webpack

--- build/webpack.js
+++ build/webpack.js
@@ -59,17 +59,9 @@ function build_dev() {
     noInfo: true,
   })
   const hotMiddleware = require('webpack-hot-middleware')(compiler, {
-    log: () => { },
+    log: false,
+    heartbeat: 2000,
   });
-  // force page reload when html-webpack-plugin template changes
-  compiler.plugin('compilation', function (compilation) {
-    compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-      hotMiddleware.publish({
-        action: 'reload'
-      })
-      cb()
-    })
-  })
 
   app.use(devMiddleware)
   app.use(hotMiddleware)
@@ -83,7 +75,7 @@ function build_dev() {
 
   console.log('> Starting dev server...')
   devMiddleware.waitUntilValid(() => {
-    const uri = 'http://localhost:' + config.dev.port
+    let uri = 'http://localhost:' + config.dev.port
     console.log('> Listening at ' + uri + '\n')
     config.dev.autoOpenBrowser && opn(uri)
   })
